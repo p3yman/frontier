44dc94bf463d2bd4190d5dff5164b058b32a9ede
Peyman Eskandari <peyman.eskandari@gmail.com>
2018-03-11 11:01:28 +0330

    better tasks

--- gulpfile.js
+++ gulpfile.js
@@ -15,13 +15,20 @@ var gulp            = require('gulp'),
     concat          = require('gulp-concat'),
     fileinclude     = require('gulp-file-include'),
     uglify          = require('gulp-uglify'),
-    csscomb         = require('gulp-csscomb');
+    csscomb         = require('gulp-csscomb'),
+    prettify        = require('gulp-prettify'),
+    zip             = require('gulp-zip'),
+    dateFormat      = require('dateformat'),
+    runSequence     = require('run-sequence'),
+    rtlcss          = require('gulp-rtlcss');
 
 
 // Directories
 //----------------------
 var src_dir         = 'src/',
     dist_dir        = 'dist/',
+    backup_dir      = 'backup/',
+    build_dir       = 'build/',
 
     html = {
         in: src_dir + '*.html',
@@ -62,12 +69,21 @@ gulp.task('styles', function() {
     return gulp.src(css.in)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 5 versions'],
-            cascade: false
-        }))
-        .pipe(sourcemaps.write('./'))
+        .pipe(autoprefixer("last 5 version"))
+        .pipe(sourcemaps.write())
+        .pipe( gulp.dest( css.out ) )
+        // .pipe(rtlcss())
+        // .pipe(rename({ suffix: '-rtl' }))
+        .pipe( gulp.dest( css.out ) );
+});
+gulp.task('stylesBuild', function() {
+    return gulp.src(css.in)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer("last 5 version"))
         .pipe(csscomb())
+        .pipe(gulp.dest(css.out))
+        .pipe( rename({suffix: '.min'}) )
+        .pipe( cssnano() )
         .pipe( gulp.dest( css.out ) );
 });
 
@@ -105,23 +121,50 @@ gulp.task('html', function() {
             prefix: '@@',
             basepath: '@file'
         }))
+        .pipe(prettify({indent_size: 4}))
         .pipe(gulp.dest(html.out));
 });
-
 // Clean all files in destination folder
 gulp.task('clean', function() {
     return gulp.src(dist_dir + '*', { read: false })
         .pipe(rimraf({ force: true }));
 });
 
-// Default and Watch
-gulp.task('default', ['styles', 'scripts-lib', 'scripts-vendor', 'images', 'fonts', 'html'], function() {
+// Backup
+gulp.task('backup', ['clean'], function() {
+    var date = new Date();
+    var name = 'backup-' + dateFormat(date, "yyyy-mm-dd--H-MM-ss") + '.zip';
+    return gulp.src([src_dir + '**/*', 'gulpfile.js', 'package.json'])
+        .pipe(zip(backup_dir + name))
+        .pipe(gulp.dest('.'));
+});
 
-    gulp.watch( css.in          , ['styles']  );
-    gulp.watch( js.in_lib       , ['scripts-lib'] );
+// Build
+gulp.task('dist', function() {
+    var date = new Date();
+    var name = 'build-' + dateFormat(date, "yyyy-mm-dd--H-MM-ss") + '.zip';
+    return gulp.src([dist_dir + '**/*'])
+        .pipe(zip(build_dir + name))
+        .pipe(gulp.dest('.'));
+});
+
+// Default and Watch
+gulp.task('default', ['styles', 'scripts-vendor', 'scripts-lib', 'images', 'fonts', 'html'], function() {
+
+    gulp.watch( css.in          , ['stylesBuild']  );
     gulp.watch( js.in_vendor    , ['scripts-vendor'] );
+    gulp.watch( js.in_lib       , ['scripts-lib'] );
     gulp.watch( images.in       , ['images']  );
     gulp.watch( fonts.in        , ['fonts']   );
     gulp.watch( html.watch      , ['html']    );
 
+});
+
+// Build
+gulp.task('build', function() {
+    runSequence(
+        'backup',
+        ['stylesBuild', 'scripts-vendor', 'scripts-lib', 'images', 'fonts', 'html'],
+        'dist'
+    );
 });
